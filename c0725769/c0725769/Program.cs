5d5d88158ab2ac80618b96c42153fbe7f1b307a5
Ramandeep  Rall <725769@student.tor.loc>
2019-03-28 15:18:44 -0400

    added download method

--- c0725769/c0725769/Program.cs
+++ c0725769/c0725769/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace c0725769
@@ -10,6 +11,13 @@ namespace c0725769
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Download();
+            Console.ReadLine();
+        }
+        static void Download() {
+            Thread.Sleep(4000);
+            Console.WriteLine("downloading start");
         }
     }
 }
